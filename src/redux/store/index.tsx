2e69c76a03c181bbeaf27d970e5605c8fe8a6d84
MonokaiJs <monokaijs@gmail.com>
2022-01-20 19:18:48 +0700

    modified auth method and added some automation in renewing credentials

--- src/redux/store/index.tsx
+++ src/redux/store/index.tsx
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import accountReducer from '@redux/reducers/account.reducer';
 import semesterReducer from "@redux/reducers/semester.reducer";
 import appReducer from "@redux/reducers/app.reducer";
 
 export const store = configureStore({
   reducer: {
-    account: accountReducer,
     semester: semesterReducer,
     app: appReducer
   }
