4be7aa09c19d036fdd0e994e03650e22f93860c5
Lê Xuân Ngọc <ngoc1997307@gmail.com>
2022-01-20 22:27:41 +0700

    fix conflict app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {setStudentInfo} from '@redux/reducers/account.reducer';
+import React, { useEffect, useState } from "react";
+import { Image, SafeAreaView, StatusBar, View } from "react-native";
 import {
-  setAppReady,
-  setAuthModalShown,
-  setLoginStatus,
-} from '@redux/reducers/app.reducer';
-import {RootState, store} from '@redux/store';
-import AuthModal from '@screens/auth';
-import DefaultAppTheme from '@theme';
-import MainTabsNavigator from 'navigations/main-tabs';
-import React, {useEffect} from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import {Provider, useDispatch, useSelector} from 'react-redux';
-import AuthService from './services/AuthService';
+  GoogleSignin,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import { NavigationContainer } from "@react-navigation/native";
+import MainTabsNavigator from "navigations/main-tabs";
+import DefaultAppTheme from "@theme";
+import ApiService from "./services/ApiService";
+import { RootState, store } from "@redux/store";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { setAvatar, setStudentInfo } from "@redux/reducers/account.reducer";
+import AuthService from "./services/AuthService";
+import { setAppReady, setAuthModalShown, setLoginStatus } from "@redux/reducers/app.reducer";
+import AuthModal from "@screens/auth";
+import { getData } from "./services/StorageService";
 
 const AppContent = () => {
-  const app = useSelector((state: RootState) => state.app);
+  const app = useSelector((state : RootState) => state.app);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -43,15 +45,15 @@ const AppContent = () => {
   }, []);
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar
         translucent
-        backgroundColor={'transparent'}
-        barStyle={'dark-content'}
+        backgroundColor={"transparent"}
+        barStyle={"dark-content"}
       />
       <NavigationContainer theme={DefaultAppTheme}>
         <MainTabsNavigator />
-        <AuthModal />
+        <AuthModal/>
       </NavigationContainer>
     </SafeAreaView>
   );
